44901de10718f50e38521a061bbf6f385ae68875
vsnation <vsnation.v@gmail.com>
2026-07-25 13:26:58 +0000

    Brand assets: dial-mark favicon set (16/32/apple-touch), maskable app icons (192/512), logo lockup, OG banner, and an instant preheat splash screen; OG/Twitter meta

--- apps/web/sw.js
+++ apps/web/sw.js
@@ -1,5 +1,5 @@
 const V = 'cooked-v17';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon-32.png', './favicon-16.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request))); });
